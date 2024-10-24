8e2e8055c1a243685ab801047679c92b887dc96e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-10-24 12:45:33 +0700

    update

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.post("/:_id/webhook", (req, res, next) => {
                 console.log(`https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=${encodeURIComponent(update.qr)}`);
             }
         }
+        
         // if (req.body["creds.update"]) console.log(req.body["creds.update"]);
         // if (req.body["messaging-history.set"]) console.log(req.body["messaging-history.set"]);
         // if (req.body["chats.upsert"]) console.log(req.body["chats.upsert"]);
@@ -31,6 +32,7 @@ app.post("/:_id/webhook", (req, res, next) => {
         // if (req.body["messages.delete"]) console.log(req.body["messages.delete"]);
         // if (req.body["messages.update"]) console.log(req.body["messages.update"]);
         // if (req.body["messages.media-update"]) console.log(req.body["messages.media-update"]);
+        
         if (req.body["messages.upsert"]) {
             const upsert = req.body["messages.upsert"];
             if (upsert.type === "notify") {
@@ -39,6 +41,7 @@ app.post("/:_id/webhook", (req, res, next) => {
                 }
             }
         }
+        
         // if (req.body["messages.reaction"]) console.log(req.body["messages.reaction"]);
         // if (req.body["message-receipt.update"]) console.log(req.body["message-receipt.update"]);
         // if (req.body["groups.upsert"]) console.log(req.body["groups.upsert"]);
@@ -47,6 +50,7 @@ app.post("/:_id/webhook", (req, res, next) => {
         // if (req.body["blocklist.set"]) console.log(req.body["blocklist.set"]);
         // if (req.body["blocklist.update"]) console.log(req.body["blocklist.update"]);
         // if (req.body["call"]) console.log(req.body["call"]);
+        
         res.json({ message: "OK" });
     } catch (error) {
         next(error);
